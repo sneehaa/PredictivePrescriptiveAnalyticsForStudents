d5d4aa1cd84c8c0695212c2abc9de7183f090f3d
Snehaa <94774019+sneehaa@users.noreply.github.com>
2024-07-24 12:20:38 +0545

    Add resources management

---
+++ resources.py
@@ -0,0 +1,28 @@
+# models.py
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Resource(db.Model):
+    __tablename__ = 'resources'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    subject = db.Column(db.String(50), nullable=False)
+    learning_style = db.Column(db.String(50), nullable=False)
+    description = db.Column(db.String(500))
+    resource_type = db.Column(db.String(50), nullable=False)
+    link = db.Column(db.String(500), nullable=False)
+    summary = db.Column(db.String(1000), nullable=True)
+    answer = db.Column(db.Text, nullable=True)  # New field for direct answers
+
+    def __init__(self, title, subject, learning_style, description, resource_type, link, summary=None, answer=None):
+        self.title = title
+        self.subject = subject
+        self.learning_style = learning_style
+        self.description = description
+        self.resource_type = resource_type
+        self.link = link
+        self.summary = summary
+        self.answer = answer
+
